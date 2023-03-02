22c1bce6f2dc47a8bc81f9f0bdb93cc488f2bb4c
abhigyandsa <abhigyandsa@gmail.com>
2023-03-03 00:14:07 +0530

    add nudge experiment

---
+++ projects/sike/script.js
@@ -0,0 +1,29 @@
+const backgrounds = ['bkgs/healthy-background.jpg', 'bkgs/junk-background.jpg'];
+const background = document.querySelector('.background');
+const form = document.querySelector('form');
+const checkboxes = document.querySelectorAll('input[name="menu-item"]');
+
+const menuitems = [
+  {name: 'Burger', type: 'Main Course', healthy: -2},
+  {name: 'Salad', type: 'Main Course', healthy: 2},
+]
+
+// Randomly load a background image
+r = Math.random();
+if (r < 0.5)
+    background.style.backgroundImage = `url(${backgrounds[0]})`;
+else
+    background.style.backgroundImage = `url(${backgrounds[1]})`;
+
+// Record the participant's order
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const order = [];
+  checkboxes.forEach((checkbox) => {
+    if (checkbox.checked) {
+      order.push(checkbox.value);
+    }
+  });
+  console.log(order);
+});
+ 
