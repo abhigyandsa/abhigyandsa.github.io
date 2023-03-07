27da693174be39a7c63264af97be0875dfb0075c
abhigyandsa <abhigyandsa@gmail.com>
2023-03-08 01:45:55 +0530

    add thank you cheat

--- projects/sike/script.js
+++ projects/sike/script.js
@@ -1,7 +1,9 @@
 const backgrounds = ['bkgs/healthy-background.jpg', 'bkgs/junk-background.jpg'];
 const background = document.querySelector('.background');
-const form = document.querySelector('form');
+// const form = document.querySelector('form');
 const checkboxes = document.querySelectorAll('input[name="menu-item"]');
+const submitButton = document.getElementById("submit-button");
+
 
 const menuitems = [
   {name: 'Burger', type: 'Main Course', healthy: -2},
@@ -16,14 +18,22 @@ else
     background.style.backgroundImage = `url(${backgrounds[1]})`;
 
 // Record the participant's order
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const order = [];
-  checkboxes.forEach((checkbox) => {
-    if (checkbox.checked) {
-      order.push(checkbox.value);
-    }
-  });
-  console.log(order);
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault();
+//   const order = [];
+//   checkboxes.forEach((checkbox) => {
+//     if (checkbox.checked) {
+//       order.push(checkbox.value);
+//     }
+//   });
+//   console.log(order);
+// });
+
+submitButton.addEventListener("click", function(event) {
+    var thank_you_page = document.getElementById("thank-you-page");
+    var menu = document.getElementById("menu-container");
+    menu.style.display = "none";
+    thank_you_page.style.visibility = "visible";
+    thank_you_page.style.display = "block";
+    console.log("Thank you for your order!");
 });
- 
